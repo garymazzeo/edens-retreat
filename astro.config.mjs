bf4b9a812ae236f2c5b788b84b525f19c89d0e8b
Gary Mazzeo <gary.mazzeo@gmail.com>
2023-05-14 22:07:34 -0400

    :wrench: Set up github actions for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-
+  site: 'https://garymazzeo.github.io',
+  base: '/edens-retreat',
 });
